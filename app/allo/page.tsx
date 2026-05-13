e9559044ee350b3d30ee1c6cb651daaede9fcfed
baloghtatailevente <baloghtatai.levente@students.jedlik.eu>
2026-05-13 10:25:34 +0200

    Added gray background

--- app/allo/page.tsx
+++ app/allo/page.tsx
@@ -13,13 +13,16 @@ const links: LinkType[] = [
 
 export default function AlapokPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-blue-200">
-      <div className="flex w-100 flex-col items-center rounded-full bg-gray-50 p-8 shadow-2xl" >
-        <h1 className="text-xl font-semibold text-center">Álló csigára ható erők kiszámítása</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex w-100 flex-col items-center rounded-full bg-gray-50 p-8 shadow-2xl">
+        <h1 className="text-center text-xl font-semibold">Álló csigára ható erők kiszámítása</h1>
         <ul className="mt-3">
           {links.map((link, index) => (
             <li key={index}>
-              <Link className="text-blue-500 font-semibold hover:font-bold hover:bg-gray-200 rounded-full px-3"  href={link.href}>
+              <Link
+                className="rounded-full px-3 font-semibold text-blue-500 hover:bg-gray-200 hover:font-bold"
+                href={link.href}
+              >
                 {link.label}
               </Link>
             </li>
